a2aa9ef4b85ab1bd54b5f0d35d8126be864d0550
heiwa4126 <heiwa4126@gmail.com>
2024-07-23 11:14:24 +0900

    add `file download`

---
+++ src/app.tsx
@@ -0,0 +1,46 @@
+import { Hono } from "hono";
+import { DateTime } from "luxon";
+
+function dtStr(dt?: DateTime | null): string {
+	return (dt ?? DateTime.local()).toFormat("yyyy-MM-dd HH:mm:ss ZZZ");
+}
+function dtStrFile(dt?: DateTime | null): string {
+	return (dt ?? DateTime.local()).toFormat("yyyyMMddHHmmss");
+}
+
+const app = new Hono();
+
+const View = () => {
+	const title = "Hello Hono!";
+	return (
+		<html lang="en">
+			<head>
+				<title>{title}</title>
+			</head>
+			<body>
+				<h1>{title}</h1>
+				<p>
+					<a href="download">click here</a>
+				</p>
+				<p>{dtStr()}</p>
+			</body>
+		</html>
+	);
+};
+
+app.get("/", (c) => {
+	return c.html(<View />);
+});
+
+app.get("/download", async (c) => {
+	const now = DateTime.local();
+
+	const csvFileName = `test1-${dtStrFile(now)}.csv`;
+	c.header("Content-Type", "text/csv");
+	c.header("Content-Encoding", "UTF-8");
+	c.header("Content-Disposition", `attachment; filename="${csvFileName}"`);
+
+	return c.body(`filename,created_at\n${csvFileName},${dtStr(now)}\n`);
+});
+
+export default app;
