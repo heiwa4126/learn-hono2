86d87293ae1fe879b607469f23450bf5ff17c2d0
heiwa4126 <heiwa4126@gmail.com>
2024-07-23 14:56:20 +0900

    [WIP] adding app test...

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,10 +2,13 @@ import { describe, expect, test } from "bun:test";
 import { testClient } from "hono/testing";
 import app, { type AppType } from "./app";
 
+function getTestClient() {
+	return testClient<AppType>(app);
+}
+
 describe("App", () => {
 	test("/(index)", async () => {
-		const client = testClient<AppType>(app);
-		const res = await client.index.$get();
+		const res = await getTestClient().index.$get();
 		expect(await res.text()).toStartWith(
 			'<html lang="en"><head><title>Hello Hono!</title></head><body><h1>Hello Hono!</h1><p><a href="download">click here</a></p><p>',
 		);
